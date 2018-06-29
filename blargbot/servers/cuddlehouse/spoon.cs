1fafb4c1ca626890707a6d187bebbf8cebcba9e0
loliprotector <kaworinakano@gmail.com>
2018-06-29 18:31:46 +0800

    Fixed switch
    Removed "true" in the default argument of switch.

--- blargbot/servers/cuddlehouse/spoon.cs
+++ blargbot/servers/cuddlehouse/spoon.cs
@@ -10,5 +10,5 @@
 {switch;true;
 	{bool;{lower;{get;~s}};includes;small};{void;{roleadd;451248163760766988}}{set;_{userid}spoon;Small Spoon};
 	{bool;{lower;{get;~s}};includes;big};{void;{roleadd;451248172300369921}}{set;_{userid}spoon;Big Spoon};
-	true;{void;{roleadd;457535407085715476}}{set;_{userid}spoon;Switch Spoon}
+	{void;{roleadd;457535407085715476}}{set;_{userid}spoon;Switch Spoon}
 }
