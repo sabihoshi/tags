80b83fc44968bed22a41c7efe20cba803ed87196
loliprotector <kaworinakano@gmail.com>
2018-06-30 18:02:29 +0800

    Make greet universal

--- blargbot/servers/doggo/greet.js
+++ blargbot/servers/doggo/greet.js
@@ -1,13 +1,4 @@
-b!farewell -c #join_log
-b!greet {set;~roles;
-    437287008394805249;
-    437291155239993374;
-    437291974765182979;
-    435128972209160192;
-    437004403833176068;
-    437240793502187521
-}
-{foreach;~role;{get;~roles};{if;{indexof;{get;_{userid}roles};{get;~role}};!=;-1;{if;{addrole;{get;~role}};{username}#{userdiscrim} was given back {rolename;{get;~role}}{newline}}}}
+b!greet {set;~log;437879937400045568}
 {set;~time;{usercreatedat;X}}
 {set;~time;{floor;{math;-;{time;X};{get;~time}}}}
 {set;~d;{floor;{math;/;{get;~time};86400}}}
@@ -15,7 +6,7 @@ b!greet {set;~roles;
 {set;~h;{floor;{math;/;{get;~time};3600}}}
 {set;~m;{floor;{math;/;{math;%;{get;~time};3600};60}}}
 {set;~s;{floor;{math;%;{get;~time};60}}}
-{void;{send;437879937400045568;{embed;{buildembed;
+{void;{send;{get;~log};{embed;{buildembed;
 author.name:{username}#{userdiscrim} ({userid});
 author.icon_url:{useravatar};
 {if;{get;~time};<=;{math;*;60;60;24};
