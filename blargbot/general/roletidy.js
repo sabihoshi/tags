0202b0fa8e0a94479b442237f47095b5c6c618e7
loliprotector <kaworinakano@gmail.com>
2018-06-30 23:53:28 +0800

    Basic snippet to tidy unwanted roles
    Styles to choose from:
    - Delete all empty roles
    - Delete all 'new role' roles
    - Delete all roles without color

--- blargbot/general/roletidy.js
+++ blargbot/general/roletidy.js
@@ -1,5 +1,22 @@
+{//; Styles to choose from:
+	0: Delete all empty roles
+	1: Delete all 'new role' roles
+	2: Delete all roles without color
+ }
+{set;~style;0}
 {foreach;~role;{roles};
-	{if;{rolemembers;{get;~role}};==;0;
-		{roledelete;{get;~role}}
+	{switch;{get~style};
+		0;
+			{if;{rolemembers;{get;~role}};==;0;
+				{roledelete;{get;~role}}
+			};
+		1;
+			{if;{rolename;{get;~role}};==;new role;
+				{roledelete;{get;~roles}}
+			};
+		2;
+			{if;{rolecolor;{get;~role}};==;000000;
+				{roledelete;{get;~roles}}
+			}
 	}
 }
