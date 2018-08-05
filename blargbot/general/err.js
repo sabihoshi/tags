7d35d9d824428b7d9cab90c1adf85bf2200ee31e
loliprotector <kaworinakano@gmail.com>
2018-08-05 20:31:41 +0800

    Add logic for CCs

--- blargbot/general/err.js
+++ blargbot/general/err.js
@@ -1,8 +1,8 @@
 {buildembed;
-	author:{username}#{userdiscrim};
-	author.icon_url:{useravatar};
-	description:{args;0};
-	footer:Error!;
-	color:red;
-	timestamp:{time}
+    author.name:{username}#{userdiscrim};
+    author.icon_url:{useravatar};
+    description:{args};
+    footer.text:Please do {prefix}{if;{logic;!;{iscc}};t{space}}{commandname} help for more info.;
+    color:red;
+    timestamp:{time}
 }
