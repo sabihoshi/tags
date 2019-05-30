2970c307bd1a62ffb2ef2ce906a8c8df3a7adf71
loliprotector <kaworinakano@gmail.com>
2019-05-30 16:28:02 +0800

    Fix math
    Fixed a bug where `(x)+5` would be changed to `(x*+5)`.
    Combined regex for `x(y)`, `(y)x`.
    Made new rule for `(x)+y`, `(x)*y`, `(x)/y`, `(x)-y`.

--- blargbot/general/math.js
+++ blargbot/general/math.js
@@ -13,8 +13,8 @@
 	{set;~f;{exec;number;{get;~f}}}
 	{exec;whileregexinject;"~f" "/(-?(?:\d+\.\d*|\.?\d+))([*/%])(-?(?:\d+\.\d*|\.?\d+))/" "{lb}lb{rb}math{lb}semi{rb}$2{lb}semi{rb}$1{lb}semi{rb}$3{lb}rb{rb}"}{//;Solve x*y, x/y, x%y}{if;{get;~v};{if;{get;~nf};!=;{get;~f};{set;~nf;{get;~f}}11 - {get;~f}}}
 	{exec;whileregexinject;"~f" "/(-?(?:\d+\.\d*|\.?\d+))([-+])(\d+\.\d*|\.?\d+)/" "{lb}lb{rb}math{lb}semi{rb}$2{lb}semi{rb}$1{lb}semi{rb}$3{lb}rb{rb}"}{//;Solve x+y, x-y}{if;{get;~v};{if;{get;~nf};!=;{get;~f};{set;~nf;{get;~f}}12 - {get;~f}}}
-	{exec;whileregex;"~f" "/\((-?(?:\d+\.\d*|\.?\d+))\)\((-?(?:\d+\.\d*|\.?\d+))\)/" "($1*$2)"}{//;Resolve (x)(y) -> (x*y)}{if;{get;~v};{if;{get;~nf};!=;{get;~f};{set;~nf;{get;~f}}13 - {get;~f}}}
-	{exec;whileregex;"~f" "/\((-?(?:\d+\.\d*|\.?\d+))\)(-?(?:\d+\.\d*|\.?\d+))/" "($1*$2)"}{//;Resolve (x)y -> (x*y)}{if;{get;~v};{if;{get;~nf};!=;{get;~f};{set;~nf;{get;~f}}14 - {get;~f}}}
-	{exec;whileregex;"~f" "/(-?(?:\d+\.\d*|\.?\d+))\((-?(?:\d+\.\d*|\.?\d+))\)/" "($1*$2)"}{//;Resolve x(y) -> (x*y)}{if;{get;~v};{if;{get;~nf};!=;{get;~f};{set;~nf;{get;~f}}15 - {get;~f}}}
+	{exec;whileregex;"~f" "/\((-?(?:\d+\.\d*|\.?\d+))\)\((-?(?:\d+\.\d*|\.?\d+))\)/g" "($1*$2)"}{//;Resolve (x)(y) -> (x*y)}{if;{get;~v};{if;{get;~nf};!=;{get;~f};{set;~nf;{get;~f}}13 - {get;~f}}}
+	{exec;whileregex;"~f" "/\((-?(?:\d+\.\d*|\.?\d+))\)(?![-+/*])(\d+\.\d*|\.?\d+)|(?![-+/*])(\d+\.\d*|\.?\d+)\((-?(?:\d+\.\d*|\.?\d+))\)/g" "($1$3*$2$4)"}{//;Resolve (x)y, y(x) -> (x*y)}{if;{get;~v};{if;{get;~nf};!=;{get;~f};{set;~nf;{get;~f}}14 - {get;~f}}}
+	{exec;whileregexinject;"~f" "/\((-?(?:\d+\.\d*|\.?\d+))\)([-+/*])(-?(?:\d+\.\d*|\.?\d+))|(-?(?:\d+\.\d*|\.?\d+))([-+/*])\((-?(?:\d+\.\d*|\.?\d+))\)/" "{lb}lb{rb}math{lb}semi{rb}$2$5{lb}semi{rb}$1$4{lb}semi{rb}$3$5{lb}rb{rb}"}{//;Solve (x)+y, (x)*y, (x)/y, (x)-y}{if;{get;~v};{if;{get;~nf};!=;{get;~f};{set;~nf;{get;~f}}15 - {get;~f}}}
 	{set;~f;{regexreplace;{get;~f};/(?:^|\s|([-+/*^(]))\((-?(?:\d+\.\d*|\.?\d+))\)/g;$1$2}}{//;Resolve (x) -> x}{if;{get;~v};{if;{get;~nf};!=;{get;~f};{set;~nf;{get;~f}}16 - {get;~f}}{newline}}
 }}{if;{get;~v};{newline}A{space}-{space}}{parsefloat;{get;~f}}{if;{get;~v};```}
