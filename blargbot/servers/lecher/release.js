0a2c22029866447b757be6d87d5e2b0b4f5a2873
loliprotector <kaworinakano@gmail.com>
2020-02-29 08:41:24 +0800

    Improve styling

--- blargbot/servers/lecher/release.js
+++ blargbot/servers/lecher/release.js
@@ -4,18 +4,43 @@
 
 {set;~embed;["author.name:{username}", "author.icon_url:{useravatar}"]}
 
-{set;~flags;{j;[
-    {"flag": "t", "key": "title"},
-    {"flag": "d", "key": "description"},
-    {"flag": "i", "key": "thumbnail.url"}
-]}}
-{foreach;~flag;~flags;
-    {if;{flagset;{jget;~flag;flag}};
-        {push;~embed;{jget;~flag;key}:{flag;{jget;~flag;flag}}}
+{//; Release details }
+{set;~title;{flag;_}}
+{set;~description;{flag;d}}
+{set;~image;{flag;i}}
+
+{//; Links }
+{set;~twitter;{flag;t}}
+{set;~e-hentai;{flag;e}}
+{set;~nhentai;{flag;n}}
+
+{set;~links;{j;[
+    {
+        "name": "twitter",
+        "emoji": "<:Twitter:682246371079880730>",
+        "link": "twitter.com"
+    },
+    {
+        "name": "e-hentai",
+        "emoji": "<:EHentai:682301919200673811>",
+        "link": "e-hentai.org"
+    },
+    {
+        "name": "nhentai",
+        "emoji": "<:Nhentai:682301919158730771>",
+        "link": "nhentai.net"
     }
+]}}
+
+{set;~embed;
+    title:{get;~title};
+    description:{foreach;~link;~links;{get;~{jget;~link;name}}{space}};
+    image:{get;~image};
+    color:{get;~color};
+
+    fields.name;Description;
+    fields.value;{get;~description};
+    fields.inline:true
 }
 
-"[<:Twitter:682246371079880730>](https://twitter.com/kami_shun0505) [<:EHentai:682301919200673811>](https://e-hentai.org/g/1503018/2db137502d/) [<:Nhentai:682301919158730771>](https://nhentai.net/g/288477/)"
-
-{delete}
 {webhook;{get;~id};{get;~key};{flag;_};{apply;embedbuild;{get;~embed}}}
